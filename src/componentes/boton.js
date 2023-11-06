a54478b4ccd0a20fa9db618a26689e640224f2fd
Juan Dluyz <juansedp2015@gmail.com>
2023-11-06 01:04:55 -0500

    Boton CSS + estructura de botones

--- src/componentes/boton.js
+++ src/componentes/boton.js
@@ -1,6 +1,12 @@
 import React from "react";
+import '../Hojas-de-estilos/Boton.css'
 
 function Boton(props){
+
+    const esOperador = valor => {
+        return isNaN(valor) && (valor != '.') && (valor != '=');
+    };
+
     return(
         <div
         className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`}>
