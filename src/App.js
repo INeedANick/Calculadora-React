c776f26f7fbd0c596afb386a969a274e9180af6f
Juan Dluyz <juansedp2015@gmail.com>
2023-11-05 21:18:08 -0500

    funcion Boton + archivo js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import logo from "./Imagenes/logo.png"
+import Boton from './componentes/boton'
 
 function App() {
   return (
@@ -12,7 +13,16 @@ function App() {
         />
       </div>
       <div className='calculadora-contenedor'>
-
+        <div className='fila'>
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
       </div>
     </div>
   );
